1c61c9c7360da42ec725e1bcd1f4c9ef4cef9b91
Wannes Salomé <mail@wannessalome.nl>
2020-03-05 20:25:27 +0100

    Add eslint, tsconfig, prettierrc and editorconfig

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  extends: ['./node_modules/poetic/config/eslint/eslint-config.js'],
+  // Add custom rules here
+  rules: {
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    'comma-dangle': ['warn', 'always-multiline'],
+    semi: ['warn', 'always'],
+    'object-curly-spacing': ['warn', 'always'],
+    quotes: ['error', 'single', { avoidEscape: true, allowTemplateLiterals: false }],
+  },
+  settings: {
+    'import/resolver': {
+      typescript: {},
+    },
+  },
+};
