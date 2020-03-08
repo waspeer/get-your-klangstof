ca128afcac1172e533844800c45e181c869b29ae
Wannes Salomé <mail@wannessalome.nl>
2020-03-09 00:55:56 +0100

    add useCode and remove unnecessary setter

--- src/modules/code/models/code/code.test.ts
+++ src/modules/code/models/code/code.test.ts
@@ -18,15 +18,6 @@ describe('Code', () => {
       expect(resultZero.isSuccess()).toBe(true);
     });
 
-    it.skip('should fail when `used` prop exceeds the maximum', () => {
-      const result = Code.create({
-        code: 'blah',
-        used: USE_LIMIT + 2,
-      });
-      expect(result.isFailure()).toBe(true);
-      expect(result.isFailure() && result.error).toEqual(Errors.exceededLimit(USE_LIMIT));
-    });
-
     it('should fail when the code is empty', () => {
       const result = Code.create({
         code: '',
@@ -54,6 +45,29 @@ describe('Code', () => {
       });
       const code = result.value;
       expect(code.isValid).toBe(true);
+    });
+  });
+
+  describe('.useCode', () => {
+    it('should update the used property', () => {
+      const code = Code.create({
+        code: 'blah',
+        used: 0,
+      }).value;
+      const result = code.useCode();
+      expect(result.isSuccess()).toBe(true);
+      expect(code.used).toBe(1);
+    });
+
+    it('should fail when the limit has been reached', () => {
+      const code = Code.create({
+        code: 'blah',
+        used: USE_LIMIT,
+      }).value;
+      const result = code.useCode();
+      expect(result.isFailure()).toBe(true);
+      expect(result.isFailure() && result.error).toEqual(Errors.exceededLimit(USE_LIMIT));
+      expect(code.used).toBe(USE_LIMIT);
     });
   });
 });
--- src/modules/code/models/code/code.ts
+++ src/modules/code/models/code/code.ts
@@ -28,15 +28,20 @@ class Code {
     return this.props.used;
   }
 
-  set used(timesUsed: number) {
-    this.props.used = timesUsed;
-  }
-
   static create({ code, used }: Props): CreateResult {
     if (used < 0) return Result.fail(Errors.invalidUsed());
     if (!code.length) return Result.fail(Errors.emptyCode());
 
     return Result.ok(new Code({ code, used }));
+  }
+
+  public useCode(): EitherResponse<Errors.ErrorTypes.ExceededLimit, void> {
+    if (this.props.used >= USE_LIMIT) {
+      return Result.fail(Errors.exceededLimit(USE_LIMIT));
+    }
+
+    this.props.used += 1;
+    return Result.ok();
   }
 }
 
