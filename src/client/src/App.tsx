544f8bcbaeb2f7a18f6a1ecce98e2be2475187c5
Wannes Salomé <mail@wannessalome.nl>
2020-03-12 10:14:47 +0100

    add frontend folder

---
+++ src/client/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import Home from './sections/Home';
+
+const App = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" component={Home} />
+      </Switch>
+    </Router>
+  );
+};
+
+export default App;
