df01b793ad5ad599e1ec043c68698e24b517de4e
Wannes Salomé <mail@wannessalome.nl>
2020-03-15 00:40:46 +0100

    change color selector implementation

--- src/client/src/style/index.ts
+++ src/client/src/style/index.ts
@@ -1,8 +1,2 @@
-import createTheme from 'styled-components-theme';
-
-import { colors } from './theme';
-
-const colorSelectors = createTheme(...Object.keys(colors));
-
 export { default as ThemeProvider } from './ThemeProvider';
-export { colorSelectors as colors };
+export { default as colors } from './colorSelectors';
