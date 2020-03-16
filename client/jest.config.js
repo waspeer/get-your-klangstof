ce9cca239370caa54c8befba776483ed67d8ab54
Wannes Salomé <mail@wannessalome.nl>
2020-03-16 13:51:14 +0100

    add root alias

--- client/jest.config.js
+++ client/jest.config.js
@@ -3,6 +3,7 @@ const path = require('path');
 module.exports = {
   moduleNameMapper: {
     '\\.svg$': '<rootDir>/test/jest/fileMock.ts',
+    '^#root/(.*)$': '<rootDir>/src/$1',
   },
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
